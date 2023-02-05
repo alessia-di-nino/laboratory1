d41d5cac3b606af66d2ac40ed82ee3fbd398d537
alessiadinino <alessia.dinino@outlook.it>
2023-02-05 17:41:08 +0100

    various changes

--- fall/fall.py
+++ fall/fall.py
@@ -2,26 +2,26 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
-t = np.loadtxt("fall_time.txt")
-h = np.loadtxt("fall_height")
-sigma_h = #da calcolare con un ciclo for
+t = np.loadtxt("./fall/fall_time.txt")
+h = np.loadtxt("./fall/fall_height.txt")
+blur_h = np.loadtxt("./fall/fall_blur-height.txt")
 
+blur_h = (blur_h * np.sqrt(2)) / 200.0
 h = h / 100.0
-sigma_h = sigma_h/100.0
 
 #fit quadratico
 
-def parabola(t, a, v0, h0)
+def parabola(t, a, v0, h0):
     return 0.5 * a * t**2.0 + v0 * t + h0
 
 plt.figure("legge oraria")
-plt.errorbar(t, h, sigma_h, fmt=".")
-popt, pcov = curve_fit(parabola, t, h, sigma=sigma_h)
+plt.errorbar(t, h, blur_h, fmt=".")
+popt, pcov = curve_fit(parabola, t, h, sigma=blur_h)
 a_hat, v0_hat, h0_hat = popt
 sigma_a, sigma_v0, sigma_h0 = np.sqrt(np.diagonal(pcov))
 print(a_hat, sigma_a, v0_hat, sigma_v0, h0_hat, sigma_h0)
 
-x = np.linspace(0.0, 0.7, 100)
+x = np.linspace(np.min(t), np.max(t), 100)
 plt.plot(x, parabola(x, *popt))
 plt.xlabel("Tempo [s]")
 plt.ylabel("Altezza [m]")
